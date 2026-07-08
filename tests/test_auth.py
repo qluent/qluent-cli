4d4a0c673b640e5af7d49bff4974576e3b99b78e
Ludvig Jakobsson <jakobssonludvig@gmail.com>
2026-07-08 16:23:55 +0200

    Default CLI login to production

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -143,6 +143,10 @@ def test_api_url_to_ui_url_development():
     assert _api_url_to_ui_url("https://api.app-development.qluent.com") == "https://app-development.qluent.com"
 
 
+def test_api_url_to_ui_url_production():
+    assert _api_url_to_ui_url("https://api.app.qluent.com") == "https://app.qluent.com"
+
+
 def test_api_url_to_ui_url_development_trailing_slash():
     assert _api_url_to_ui_url("https://api.app-development.qluent.com/") == "https://app-development.qluent.com"
 
